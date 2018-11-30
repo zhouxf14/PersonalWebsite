4af2d7e6c528faafcaab1833a0ce8ac93478b3eb
zhouxf14 <zhouxf14@163.com>
2018-11-30 01:56:36 -0500

    update

--- js/one.js
+++ js/one.js
@@ -14,22 +14,23 @@ $(document).ready(function() {
 //   // document.getElementById("intro").style.top = scrollstart + "px";
 // });
 
-// var pathEls = document.querySelectorAll('.anim path');
-// for (var i = 0; i < pathEls.length; i++) {
-//   var pathEl = pathEls[i];
-//   var offset = anime.setDashoffset(pathEl);
-//   pathEl.setAttribute('stroke-dashoffset', offset);
-//   anime({
-//     targets: pathEl,
-//     strokeDashoffset: [offset, 0],
-//     duration: anime.random(1000, 3000),
-//     delay: anime.random(0, 2000),
-//     loop: true,
-//     direction: 'alternate',
-//     easing: 'easeInOutSine',
-//     autoplay: true
-//   });
-// }
+var pathEls = document.querySelectorAll('.anim path');
+for (var i = 0; i < pathEls.length; i++) {
+  var pathEl = pathEls[i];
+  var offset = anime.setDashoffset(pathEl);
+  pathEl.setAttribute('stroke-dashoffset', offset);
+  anime({
+    targets: pathEl,
+    strokeDashoffset: [offset, 0],
+    // duration: anime.random(1000, 3000),
+    duration: 1500,
+    delay: anime.random(0, 2000),
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutSine',
+    autoplay: true
+  });
+}
 
 // var paths = [
 //   {id: '#path5419', d: 'm 574.27172,479 0,-15.65736 -32.82996,4.54569 z'},
